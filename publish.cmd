52822010e6ae39fe5f226642c277a609f6e95720
AndreMachado94 <andremachado94@gmail.com>
2018-09-17 14:47:17 +0100

    Added base chatbot template

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=glintttest-Web-Deploy.pubxml -p:Password=j7magG0Ev5gWCdegE1bgk8S0du8uoxkpJnR5gwRvlL2fkgqP8pdbxje2zFPu
+
